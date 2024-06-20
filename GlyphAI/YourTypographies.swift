1363eb4746fe44e8e56a6ec2fcabe2bf6c3d8dbb
robertacordeirosampf <robertacordeirosampf@gmail.com>
2024-06-20 16:51:17 -0300

    YourTypographiesView

--- GlyphAI/YourTypographies.swift
+++ GlyphAI/YourTypographies.swift
@@ -6,45 +6,70 @@ import SwiftUI
 
 struct YourTypographies: View {
     var body: some View {
+        
         ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 0.95, green: 0.95, blue: 0.95))
+                .frame(height: 250)
+                .overlay(
                     Rectangle()
-                        .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
-                        .frame(height: 250)
-                        .overlay(
-                            Rectangle()
-                                .inset(by: 2.5)
-                                .stroke(Color.black, lineWidth: 5)
-                        )
+                        .inset(by: 2.5)
+                        .stroke(Color.black, lineWidth: 5)
+                )
             VStack {
                 HStack {
                     Text("Your typographies")
-                        .font(Font.custom("PixeloidSans-Bold", size: 18).weight(.bold))
+                        .font(Font.custom("PixeloidSans-Bold", size: 40).weight(.bold))
                         .padding(.leading, 100)
-                        .padding(.top, 25)
+                        .padding(.top, 60)
+                        .padding(.bottom, 5)
                     Spacer()
                     
                 }
-                HStack {
-                    Text("Hi")
-                        .font(Font.custom("PixeloidSans-Bold", size: 18).weight(.bold))
+//                HStack {
+//                    Text("Create personalized bitmap fonts effortlessly with intuitive drawing tools and AI-generated character sets.")
+//                        .font(Font.custom("PixeloidSans-Bold", size: 30).weight(.bold))
+//                        .padding(.leading, 100)
+//                        
+//                        
+//                    
+//                    Spacer()
+//                    
+//                }
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .edgesIgnoringSafeArea(.all)
+        
+        VStack {
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image("plus")
                         .padding(.leading, 100)
-//                        .onAppear {
-//                            for family in UIFont.familyNames.sorted() {
-//                                let names = UIFont.fontNames(forFamilyName: family)
-//                                print("Family: \(family) Font names: \(names)")
-//                            }
-//                        }
-//                        .padding(.top, 25)
-                    Spacer()
-                    Spacer()
+                })
+               
+                Spacer()
+              
+                Text("No added fontes yet")
+                   .font(Font.custom("PixeloidSans-Bold", size: 34).weight(.bold))
+                   .foregroundColor(Color(red: 0.6, green: 0.6, blue: 0.6))
+
+
+                Spacer()
+                Image("pluswhite")
+                    .padding(.trailing, 100)
+                    .foregroundColor(.white)
+
+          
                     
-                }
+              
             }
-                   
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading) // Alinha ao topo e à esquerda
-                .edgesIgnoringSafeArea(.all) // Ignora a área segura para ocupar toda a tela
-            }
+            Spacer()
+        }
+        
+        
+    }
 }
 
 #Preview {
