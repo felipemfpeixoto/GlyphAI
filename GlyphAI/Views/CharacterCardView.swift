a6c144557df4804eb7d02e58f76c7683f336e2c1
robertacordeirosampf <robertacordeirosampf@gmail.com>
2024-06-23 12:02:44 -0300

    grid funcionando

--- GlyphAI/Views/CharacterCardView.swift
+++ GlyphAI/Views/CharacterCardView.swift
@@ -5,51 +5,48 @@ import SwiftUI
 
 struct CharacterCardView: View {
     
-    let characters: [String] = (65...90).map { String(UnicodeScalar($0)!) } // A-Z
+    let character: String
+    let fontIndex: Int
+    let characterIndex: Int
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: Array(repeating: GridItem(.fixed(196), spacing: 16), count: 5), spacing: 16) {
-                ForEach(characters, id: \.self) { character in
-                    ZStack(alignment: .bottom) {
+        NavigationLink(destination: CanvasView(fontIndex: fontIndex, characterIndex: characterIndex)) {
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .foregroundColor(.lightGray)
+                    .frame(width: 196, height: 196)
+                    .overlay(
                         Rectangle()
-                            .foregroundColor(.lightGray)
+                            .stroke(Color.black, lineWidth: 8)
                             .frame(width: 196, height: 196)
-                            .overlay(
-                                Rectangle()
-                                    .inset(by: 7)
-                                    .stroke(Color.black, lineWidth: 5)
-                            )
-                        
-                        VStack {
-                            Spacer()
-                            Text(character)
-                                .font(Font.custom("PixeloidSans-Bold", size: 48).weight(.bold))
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                        
-                        Text(character)
-                            .font(Font.custom("PixeloidSans-Bold", size: 24).weight(.bold))
-                            .padding(.horizontal, 10)
-                            .frame(width: 180)
-                            .foregroundColor(.black)
-                            .padding(.vertical, 10)
-                            .background(Color.mediumGray)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 0)
-                                    .stroke(Color.black, lineWidth: 2.5)
-                            )
-                            .padding(.bottom, 3)
-                    }
+                    )
+                
+                VStack {
+                    Spacer()
+                    Text(character)
+                        .font(Font.custom("PixeloidSans-Bold", size: 48).weight(.bold))
+                        .foregroundColor(.black)
+                    Spacer()
                 }
+                
+                Text(character)
+                    .font(Font.custom("PixeloidSans-Bold", size: 24).weight(.bold))
+                    .padding(.horizontal, 10)
+                    .frame(width: 180)
+                    .foregroundColor(.black)
+                    .padding(.vertical, 10)
+                    .background(Color.mediumGray)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 0)
+                            .stroke(Color.black, lineWidth: 2.5)
+                    )
+                    .padding(.bottom, 3)
             }
-            .padding()
         }
-        .navigationTitle("Character Grid")
     }
 }
 
-#Preview {
-    CharacterCardView()
-}
+
+//#Preview {
+//    CharacterCardView()
+//}
--- GlyphAI/Views/CharactersView.swift
+++ GlyphAI/Views/CharactersView.swift
@@ -1,28 +1,19 @@
-//
-//  CaractersView.swift
-//  GlyphAI
-//
-//  Created by Roberta Cordeiro on 21/06/24.
-//
+
+
+
+
 
 import SwiftUI
 
 struct CharactersView: View {
     
-    @State var fonte: Typographie = Typographie(name: "", characters: [])
-    
-    let letrasMaiusculas: [String] = [
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z",
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-        ]
-    
-    let itemsPerRow = 5
-    let spacing: CGFloat = 30 // Espaçamento entre os quadrados
-    let itemSize: CGFloat = UIScreen.main.bounds.width / 6 // Tamanho dos quadrados
-    
+    @State var fonte: Typographie
     let index: Int
+    
+    init(index: Int) {
+        self.index = index
+        self.fonte = dao.fonts[index]
+    }
     
     var body: some View {
         ZStack {
@@ -34,48 +25,46 @@ struct CharactersView: View {
                 }
             }
         }
-//        .navigationBarBackButtonHidden()
         .ignoresSafeArea()
-        .onAppear {
-            fonte = dao.fonts[index]
-        }
     }
     
     var header: some View {
-            ZStack {
-                Rectangle()
-                    .foregroundColor(Color(red: 0.95, green: 0.95, blue: 0.95))
-                    .frame(height: 250)
-                    .overlay(
-                        Rectangle()
-                            .inset(by: 2.5)
-                            .stroke(Color.black, lineWidth: 5)
-                    )
-                VStack {
-                    HStack {
-                        Text(fonte.name)
-                            .font(Font.custom("PixeloidSans-Bold", size: 40).weight(.bold))
-                            .padding(.leading, 100)
-                            .padding(.top, 60)
-                            .padding(.bottom, 5)
-                        Spacer()
-                        
-                    }
-                    
-                }
-            }
-    }
-    
-    var charactersGrid: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(.fixed(itemSize), spacing: spacing), count: Int(itemsPerRow)), spacing: spacing) {
-            ForEach(fonte.characters.indices) { characterIndex in
-                NavigationLink(destination: CanvasView(fontIndex: index, characterIndex: characterIndex)) {
-                    Text(fonte.characters[characterIndex].letra)
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 0.95, green: 0.95, blue: 0.95))
+                .frame(height: 250)
+                .overlay(
+                    Rectangle()
+                        .inset(by: 2.5)
+                        .stroke(Color.black, lineWidth: 5)
+                )
+            VStack {
+                HStack {
+                    Text(fonte.name)
+                        .font(Font.custom("PixeloidSans-Bold", size: 40).weight(.bold))
+                        .padding(.leading, 100)
+                        .padding(.top, 60)
+                        .padding(.bottom, 5)
+                    Spacer()
                 }
             }
         }
     }
+    
+    var charactersGrid: some View {
+        ScrollView {
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(196), spacing: 16), count: 5), spacing: 16) {
+                ForEach(fonte.characters.indices, id: \.self) { characterIndex in
+                    CharacterCardView(character: fonte.characters[characterIndex].letra, fontIndex: index, characterIndex: characterIndex)
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Character Grid")
+    }
 }
+
+
 
 //#Preview {
 //    CharactersView()
